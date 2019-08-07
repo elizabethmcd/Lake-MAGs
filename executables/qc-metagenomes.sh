b8243c09dae6c9f8074003cba4cefebc0ad314ed
emcdaniel <emcdaniel@submit2.chtc.wisc.edu>
2019-08-07 13:12:19 -0500

    updating qc pipeline

--- executables/qc-metagenomes.sh
+++ executables/qc-metagenomes.sh
@@ -13,14 +13,11 @@ tar -xvzf BBMap_38.07.tar.gz
 
 # Copy over metagenomic read files from Gluster and decompress
 cp $1 metagenomes/
-cd metagenomes
-gunzip $metabase
-cd ..
-metarun=$(basename $metabase .fastq.gz)
+metarun=$(basename $metabase .fastq)
 
 # Filter the reads
 bbmap/bbduk.sh in=metagenomes/$metarun.fastq out=metagenomes/$metarun.qced.fastq qtrim=r trimq=10 maq=10
 
 # Move back only the sorted BAM files to Gluster for binning purposes
 tar -czvf metagenomes/$metarun.qced.fastq.tar.gz metagenomes/$metarun.qced.fastq
-cp metagenomes/$metarun.qced.fastq.tar.gz /mnt/gluster/emcdaniel/GEODES/
+cp metagenomes/$metarun.qced.fastq.tar.gz /mnt/gluster/emcdaniel/MENDOTA/
